e997d312cea965365f7f21d72f4d1c65a70914f5
Federico M. Stefanini <federico.stefanini@unifi.it>
2020-08-25 21:23:15 +0200

    Improved documentation of data.frames.

---
+++ R/penguinsDF.R
@@ -0,0 +1,30 @@
+#' Caso di studio penguinsDF
+#'
+#' Un dataset che contiene il caso di studio penguinsDF. 
+#'
+#' @usage data(penguinsDF, package="FMStatR")
+#' @usage penguinsDF
+#' @references  \url{https://allisonhorst.github.io/palmerpenguins/}
+#' @references  Palmer Station Antarctica LTER and K. Gorman, 2020. 
+#' Structural size measurements and isotopic signatures of foraging among 
+#' adult male and female Adélie penguins (Pygoscelis adeliae) nesting along 
+#' the Palmer Archipelago near Palmer Station, 2007-2009 ver 5. Environmental
+#'  Data Initiative. 
+#'  \url{https://doi.org/10.6073/pasta/98b16d7d563f265cb52372c8ca99e60f} 
+#'  (Accessed 2020-06-08).
+#' @references Palmer Station Antarctica LTER and K. Gorman, 2020. Structural size measurements 
+#' and isotopic signatures of foraging among adult male and female Gentoo 
+#' penguin (Pygoscelis papua) nesting along the Palmer Archipelago near Palmer
+#'  Station, 2007-2009 ver 5. Environmental Data Initiative. ù
+#'  \url{https://doi.org/10.6073/pasta/7fca67fb28d56ee2ffa3d9370ebda689} 
+#'  (Accessed 2020-06-08).
+#' @references Palmer Station Antarctica LTER and K. Gorman, 2020. Structural 
+#' size measurements and isotopic signatures of foraging among adult male and 
+#' female Chinstrap penguin (Pygoscelis antarcticus) nesting along the Palmer 
+#' Archipelago near Palmer Station, 2007-2009 ver 6. Environmental Data 
+#' Initiative. 
+#' \url{https://doi.org/10.6073/pasta/c14dfcfada8ea13a17536e73eb6fbe9e} 
+#' (Accessed 2020-06-08)
+#' 
+#' 
+"penguinsDF"
---
+++ R/riceDF.R
@@ -0,0 +1,14 @@
+#' Caso di studio riceDF
+#'
+#' Un dataset che contiene il caso di studio riceDF.
+#'
+#' @usage data(riceDF, package="FMStatR")
+#' @usage riceDF
+#'
+#' @references  \url{https://cran.r-project.org/web/packages/DAAG/index.html}
+#' @references Perrine, F.M., Prayitno, J., Weinman, J.J., Dazzo, F.B. and 
+#' Rolfe, B. 2001. Rhizobium plasmids are involved in the inhibition or 
+#' stimulation of rice growth and development.
+#'  Australian Journal of Plant Physiology 28: 923-927. 
+#'
+"riceDF"
